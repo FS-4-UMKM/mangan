9e30e01dea0b8d29a578fb335deba4ee999322b1
firman1703 <mfirman045@gmail.com>
2023-10-21 19:26:36 +0700

    logic tambahan

--- pages/firman-login/login.js
+++ pages/firman-login/login.js
@@ -2,18 +2,19 @@ function login(e) {
     event.preventDefault();
     const email = document.getElementById("email").value;
     const pass = document.getElementById("password").value;
-    const result = document.getElementById("result");
+
 
     const user = localStorage.getItem(email);
     const data = JSON.parse(user);
     console.log(data);
 
     if (user == null) {
-        result.innerHTML = "1";
+        alert("Data Tidak Benar");
     } else if (email == data.email && pass == data.password) {
-        result.innerHTML = "2"
+        alert("Selamat Kamu Berhasil login");
+        window.location.href = "../galang-list-produk/list-produk.html";
     } else {
-        result.innerHTML = "3"
+        alert("Password Salah");
     }
 
 
